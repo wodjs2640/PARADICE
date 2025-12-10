0a05b0db89c7e4860e7accdce9878e7dcc20df3b
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2025-12-10 20:43:41 +0900

    기본 스크립트 추가

---
+++ Assets/Scripts/DiceTemplate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceTemplate : MonoBehaviour
+{
+    public int[] eye = new int[6];
+    public GameObject[] eyes = new GameObject[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Roll()
+    {
+
+    }
+}
